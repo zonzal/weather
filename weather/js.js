31447fd95beceee98b26f2849104063c4fd94869
zonzal <96705066+zonzal@users.noreply.github.com>
2022-03-30 13:49:47 +0900

    Update js.js

--- weather/js.js
+++ weather/js.js
@@ -23,6 +23,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed : " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage= "url(https://source.unsplash.com/3840x2160/?" + name + ")"
     },
 
     search: function() {
